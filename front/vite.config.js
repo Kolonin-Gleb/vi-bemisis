afa91bae9f14dac54674891159a46f9d0e365c56
Колонин Глеб <topsecretway@yandex.ru>
2025-09-13 15:35:39 +0300

    Update vite.config.js

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,        // или '0.0.0.0' → доступен из локалки
+    port: 5173,        // порт, который хочешь открыть
+    strictPort: true,  // если занят — не будет автопереключения
+    cors: true,        // если нужен доступ из других устройств
+  },
 })
